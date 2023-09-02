c397a7eabe5d0e54a469a784c3a6bdae9c914465
Lin Zhu <lin.zhu@sjsu.edu>
2023-09-02 01:07:29 -0700

    Enable shadow on the newly added spot light. Enable receive shadows on the mashes in the scene using shadow generator

--- src/BabylonExamples/LightsShadows.ts
+++ src/BabylonExamples/LightsShadows.ts
@@ -1,4 +1,4 @@
-import { Scene, Engine, FreeCamera, Vector3, SceneLoader, AbstractMesh, MeshBuilder, Light, LightGizmo, GizmoManager, HemisphericLight } from "@babylonjs/core"
+import { Scene, Engine, FreeCamera, Vector3, SceneLoader, AbstractMesh, MeshBuilder, Light, LightGizmo, GizmoManager, SpotLight, ShadowGenerator } from "@babylonjs/core"
 import "@babylonjs/loaders";
 
 export class LightsShadows {
@@ -27,8 +27,6 @@ export class LightsShadows {
         camera.speed = 0.10;
         camera.minZ = 0.1;
 
-
-
         return scene;
     }
 
@@ -38,7 +36,7 @@ export class LightsShadows {
 
         this.models = meshes;
 
-        this.ball = MeshBuilder.CreateSphere("ball", { diameter: 1 }, this.scene);
+        this.ball = MeshBuilder.CreateSphere("ball", { diameter: 0.5 }, this.scene);
 
         this.ball.position = new Vector3(0, 1, -3);
 
@@ -47,8 +45,31 @@ export class LightsShadows {
     }
 
     CreateLights(): void {
-        const hemiLight = new HemisphericLight("hemiLight", new Vector3(0, 1, 0), this.scene);
+        //const hemiLight = new HemisphericLight("hemiLight", new Vector3(0, 1, 0), this.scene);
+
+        //const directionalLight = new DirectionalLight("directionalLight", new Vector3(0, -1, 0), this.scene);
+
+        const spotLight = new SpotLight("spotLight", new Vector3(0, 0.5, -5), new Vector3(0, 1, 3), Math.PI/2, 10, this.scene);
+
+        spotLight.intensity = 100;
+
+        spotLight.shadowEnabled = true;
+
+        const shadowGenerator = new ShadowGenerator(2048, spotLight);
+
+        this.ball.receiveShadows = true;
+        shadowGenerator.addShadowCaster(this.ball);
+
+        this.models.map((mesh) => {
+            mesh.receiveShadows = true;
+            shadowGenerator.addShadowCaster(mesh);
+        })
+
+        this.CreateGizmos(spotLight);
     }
+
+    
+
 
     CreateGizmos(customLight: Light): void {
         const lightGizmo = new LightGizmo();
